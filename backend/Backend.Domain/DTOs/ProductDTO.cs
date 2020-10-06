d475290f7a558ed0fc6ce61f6f7e7fe3e9b4ff2b
Marcos Rocha <mpaulloalves@gmail.com>
2020-10-06 14:34:57 -0300

    v2 - DTO

--- backend/Backend.Domain/DTOs/ProductDTO.cs
+++ backend/Backend.Domain/DTOs/ProductDTO.cs
@@ -6,6 +6,7 @@ namespace Backend.Domain.DTOs
 {
     public class ProductDTO
     {
+        // DTO
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
